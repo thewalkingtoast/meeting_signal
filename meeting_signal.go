e43a47ec616675322d27eac5b3cebd6d383a08b9
Adam Radabaugh <adam@walkingtoast.com>
2019-01-09 09:09:06 -0800

    Add editorconfig and format files.

--- meeting_signal.go
+++ meeting_signal.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -20,46 +20,46 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		formErr := r.ParseForm()
-    if formErr != nil {
-      log.Fatal(formErr)
-    }
-		
+		if formErr != nil {
+			log.Fatal(formErr)
+		}
+
 		switchAction := r.FormValue("switch")
-		
+
 		if switchAction == "on" {
 			// fmt.Println("Switching light to on!")
-			
+
 			_, onCmdErr := exec.Command("sh", "-c", "sudo ykushcmd ykushxs -u 0").Output()
-			
+
 			if onCmdErr != nil {
 				log.Fatal(onCmdErr)
 			}
 		} else if switchAction == "off" {
 			// fmt.Println("Switching light to off!")
-			
+
 			_, offCmdErr := exec.Command("sh", "-c", "sudo ykushcmd ykushxs -d 0").Output()
-			
+
 			if offCmdErr != nil {
 				log.Fatal(offCmdErr)
 			}
 		}
-		
+
 		out, cmdErr := exec.Command("sh", "-c", "sudo ykushcmd ykushxs -g").Output()
 
 		if cmdErr != nil {
 			log.Fatal(cmdErr)
 		}
-		
+
 		// Convert bytes array to a string
 		responseOutput := string(out[:])
-		
+
 		response := strings.TrimSpace(responseOutput)
 		status := response == "Downstream port is ON"
-		
+
 		data := MeetingSignalData{
 			LightStatus: status,
 		}
-		
+
 		tmpl.Execute(w, data)
 	})
 
